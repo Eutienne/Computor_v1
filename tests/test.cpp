c78bb1618e66daf9e389ba28cbb42573c1f68770
Your Name <eutiennerodrigues@gmail.com>
2021-10-08 22:58:41 +0200

    cmake

---
+++ tests/test.cpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   test.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: eutrodri <eutrodri@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/10/08 20:55:08 by eutrodri      #+#    #+#                 */
+/*   Updated: 2021/10/08 20:55:24 by eutrodri      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+
+    return (RUN_ALL_TESTS());
+}
---
+++ tests/test_computor.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   test_computor.cpp                                  :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: eutrodri <eutrodri@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/10/08 21:08:57 by eutrodri      #+#    #+#                 */
+/*   Updated: 2021/10/08 22:13:43 by eutrodri      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <gtest/gtest.h>
+#include "../v1.hpp"
+
+TEST(test_computor, string)
+{
+    std::string str = "5 * X^0 + 4 * X^1 + 0 * X^2 = 4 * X^0";
+    std::string::const_iterator it = str.begin();
+    v1  V(str);
+
+    ASSERT_EQ(V.find_degree(it), "0");
+}
+
+TEST(test_computor, string2)
+{
+    std::string str = "5 * X^1 + 4 * X^1 + 0 * X^2 = 4 * X^0";
+    std::string::const_iterator it = str.begin();
+    v1  V(str);
+
+    ASSERT_EQ(V.find_degree(it), "1");
+}
