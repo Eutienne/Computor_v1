a0c4a85374ad4285e0f5f5cc0ac4939931f0f8cd
Eutienne Rodrigues <eutrodri@f1r3s16.codam.nl>
2021-10-19 20:07:51 +0200

    checker in construction

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,45 @@
 /*   By: eutrodri <eutrodri@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/09/10 23:05:32 by eutrodri      #+#    #+#                 */
-/*   Updated: 2021/10/15 13:25:37 by eutrodri      ########   odam.nl         */
+/*   Updated: 2021/10/19 20:04:22 by eutrodri      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/v1.hpp"
 
+void all_spaces(std::string const & s)
+{
+   for (std::string::const_iterator it = s.begin(); it != s.end(); it++)
+   {
+      if( *it != ' ' )
+        return ;
+   }
+   throw std::runtime_error("ERROR: ALL whitespaces");
+}
+
+void    checker(std::string const & s)
+{
+    try{
+        all_spaces(s);
+        // for(std::string::const_iterator it = s.begin(); it != s.end(); it++)
+        // {
+
+
+        // }
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        exit(-1);
+    }
+   
+}
+
 int main(int argc, char** argv)
 {
     if (argc != 2)
         return 0;
+    checker(argv[1]);
     v1  V(argv[1]);
     float   sqr;
 
